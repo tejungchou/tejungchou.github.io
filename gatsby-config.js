f0631b288483a0ad4cb73d9f9d202f543b125067
tejungchou <roger04130413@gmail.com>
2023-06-17 01:08:13 -0700

    modify config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
           favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "My Minimal Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
-          startUrl: "/portfolio", // Used in manifest.json
+          startUrl: "./", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
           display: "minimal-ui", // Used in manifest.json
